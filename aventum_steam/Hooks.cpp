b8907013a33143219a99b5d557901c9edb6cc8c0
An0nimosus <121900074+An0nimosus@users.noreply.github.com>
2023-03-04 22:05:19 +0500

    Add files via upload

---
+++ aventum_steam/Hooks.cpp
@@ -0,0 +1,58 @@
+#include "Hooks.hpp"
+#include "Utils.hpp"
+
+#include "MinHook/minhook.hpp"
+
+#include <filesystem>
+#include <sstream>
+
+BOOL __stdcall Hooks::hkCreateProcessW( LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcAttr,
+	LPSECURITY_ATTRIBUTES lpThreadAttr, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+	LPCWSTR lpCurrentDir, LPSTARTUPINFOW pStartupInfo, LPPROCESS_INFORMATION pProcessInfo )
+{
+
+	std::wstring wsApplicationName = std::wstring( lpCommandLine );
+
+	std::string sApplicationName;
+	g_Utils.WstringToString( wsApplicationName, sApplicationName );
+
+	if ( !strstr( sApplicationName.c_str( ), "Counter-Strike Global Offensive\\csgo.exe" ) )
+	{
+		return o_CreateProcessW( lpApplicationName, lpCommandLine, lpProcAttr, lpThreadAttr, bInheritHandles,
+			dwCreationFlags, lpEnvironment, lpCurrentDir, pStartupInfo, pProcessInfo );
+	}
+
+	BOOL bResult = o_CreateProcessW( lpApplicationName, lpCommandLine, lpProcAttr, lpThreadAttr, bInheritHandles,
+		dwCreationFlags, lpEnvironment, lpCurrentDir, pStartupInfo, pProcessInfo );
+	if ( !bResult )
+		return bResult;
+
+	HANDLE hProcess = pProcessInfo->hProcess;
+	if( !hProcess )
+	{
+		MessageBoxA( 0, "Failed to init memory manager [0]", "Critical Error", MB_ICONERROR );
+		return bResult;
+	}
+
+	/*std::vector< uint8_t > aBinary = { };
+	g_Utils.OpenBinary("C:\\csgo_7FA90000.bin", aBinary );
+
+	void* pBase = VirtualAllocEx( hProcess, reinterpret_cast< void* >( 0x7FA90000 ), aBinary.size( ), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE );
+	if ( !pBase )
+	{
+		printf( "failed to alloc mem\n" );
+		return bResult;
+	}
+
+	WriteProcessMemory( hProcess, pBase, aBinary.data( ), aBinary.size( ), 0 );*/
+
+	if (!VirtualAllocEx(hProcess, reinterpret_cast<void*>(0x35650000), 0x7C0000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE))
+	{
+		printf("failed to alloc mem\n");
+		return bResult;
+	}
+
+	printf( "allocated!\n" );
+
+	return bResult;
+}
---
+++ aventum_steam/Install.cpp
@@ -0,0 +1,32 @@
+#include "Install.hpp"
+#include "Hooks.hpp"
+
+#include "MinHook/minhook.hpp"
+
+#include <iostream>
+
+void CInstall::Init( )
+{
+
+	if ( MH_Initialize( ) != MH_OK )
+	{
+		printf( "failed to init hooking engine\n" );
+		return;
+	}
+
+	if ( MH_CreateHookApi( L"kernel32.dll", "CreateProcessW", reinterpret_cast< void* >( Hooks::hkCreateProcessW ),
+		reinterpret_cast< void** >( &Hooks::o_CreateProcessW ) ) != MH_OK )
+	{
+		printf( "failed to init hooking engine\n" );
+		return;
+	}
+	
+	if ( MH_EnableHook( MH_ALL_HOOKS ) != MH_OK )
+	{
+		printf( "failed to set hooking engine\n" );
+		return;
+	}
+
+	printf( "hooked!\n" );
+
+}
